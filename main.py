6bb28c65df2e2a8343949e7447fd1165aa03c37f
David Mendez Guardado <demg@outlook.com>
2018-05-30 17:36:56 -0500

    -Step 4: Font Changer

--- main.py
+++ main.py
@@ -11,10 +11,28 @@ def saveAs():
     file1.close()
 
 
+def FontHelvetica():
+    global text
+    text.config(font="Helvetica")
+
+
+def FontCourier():
+    global text
+    text.config(font="Courier")
+
+
 root = Tk()
 root.title('Text Editor')
 text = Text(root)
 text.grid()
 button = Button(root, text="Save", command=saveAs)
 button.grid()
+font = Menubutton(root, text="Font")
+font.grid()
+font.menu = Menu(font, tearoff=0)
+font['menu'] = font.menu
+helvetica = IntVar()
+courier = IntVar()
+font.menu.add_checkbutton(label="Courier", variable=courier, command=FontCourier)
+font.menu.add_checkbutton(label="Helvetica", variable=helvetica, command=FontHelvetica)
 root.mainloop()
