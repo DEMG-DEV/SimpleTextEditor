27b31af1a3248900da13188c6076c4e8e58985c0
David Mendez Guardado <demg@outlook.com>
2018-05-30 17:31:54 -0500

    -Step 3: Saving Your Text

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from tkinter import *
+from tkinter import filedialog
+
+
+def saveAs():
+    global text
+    t = text.get("1.0", "end-1c")
+    savelocation = filedialog.asksaveasfilename()
+    file1 = open(savelocation, "w+")
+    file1.write(t)
+    file1.close()
+
 
 root = Tk()
 root.title('Text Editor')
 text = Text(root)
 text.grid()
+button = Button(root, text="Save", command=saveAs)
+button.grid()
 root.mainloop()
